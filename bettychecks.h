bd8d9ff2ca426cd3bfa3537294bde8dc6b464cdd
Rosemwende <rosemuema82@gmail.com>
2024-03-05 22:32:44 -0800

    Beautiful code that passes the betty checks

---
+++ bettychecks.h
@@ -0,0 +1,17 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+
+#include <stdio.h>
+/**
+ * main - entry point for the program of simple_shell
+ *
+ * Return: Always 0 if successful
+ * Description: The function as the entry point
+ * of the program of simple_shell and it returns 0
+ * the execution is successful.
+ */
+int main(void)
+{
+return (0);
+}
+#endif /*SIMPLE_SHELL_H*/
